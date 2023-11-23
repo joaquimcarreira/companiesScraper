0f538b588a17d7c3cace8387b82ef25c8cb12360
joaquim <jcarreira30@gmail.com>
2023-11-24 05:04:48 +0900

    add readme

--- companiesScraper.py
+++ companiesScraper.py
@@ -16,33 +16,35 @@ class Companies():
         self.URL = f'https://stockrow.com/api/companies/'
         _requests_indicators = requests.get("https://stockrow.com/api/indicators.json").text
         self.indicators = pd.DataFrame(json.loads(_requests_indicators))
-   
+
         if self.tickers==None:
             #if tickers have not been set manually, call the screener
             self._companies_screener()  
+
         pass
     
     def _companies_screener(self):
-        api_request = (requests.get(f"https://financialmodelingprep.com/api/v3/stock-screener?apikey={fin_prep_api}",
+        api_request = (requests.get(f"https://financialmodelingprep.com/api/v3/stock-screener?apikey={self.key}",
                                     params=self.screner_data))
         data_json = json.loads(api_request.text)
         self.tickers = [company["symbol"] for company in data_json]
-        
+
+
     def get_data(self,financial,output_path=None,period="A"):
             fin = []    
             financial_to_download = self._process_financial_input(financial)
 
             if type(self.tickers) == list:
                 for ticker in tqdm(self.tickers):
-                    print(f'DOWNLOADING {financial_to_download}')
-                    income_url = self.URL+f'{ticker}/financials.json?ticker={ticker}&dimension={period}&section={financial_to_download}'
+
+                    url = self.URL+f'{ticker}/financials.json?ticker={ticker}&dimension={period}&section={financial_to_download}'
                     #get the data
-                    api_requests = requests.get(income_url)                
+                    api_requests = requests.get(url)                
                     #process data if response is ok
                     if api_requests.status_code == 200:
                         #get the json and create a df
                         data = json.loads(api_requests.text)
-                        data = pd.DataFrame(data)
+                        #data = pd.DataFrame(data)
                         # preprocess the response of the api for posterior convinience
                         try:
                             data = self.pipe(data,ticker,financial)
@@ -54,7 +56,7 @@ class Companies():
                             
                         time.sleep(0.3)
                     else: self.error_companies.append(ticker)    
-            data = pd.concat([d for d in fin])
+            data = pd.concat([d for d in fin])     
             if output_path:
                 self.save_as_csv(data,output_path)
             return data
@@ -67,7 +69,7 @@ class Companies():
                 financial_to_download = "Balance+Sheet"
             elif financial.lower() in "metrics" or financial.lower() in "ratios":
                 financial_to_download = "Metrics" 
-                scale = False
+                
             else: financial_to_download = "Cash+Flow" 
             return financial_to_download
     
@@ -97,7 +99,8 @@ class Companies():
         #change the name index of the columns
         df.columns.name = financial
         return df
-    
+
+
     def pipe(self,data,ticker,financial):
         # steps of data preprocessing
         df = (
